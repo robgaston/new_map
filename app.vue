ebfe41c4573f740feda1cb03120f5b6e57e3be3e
Rob Gaston <rgaston@fargeo.com>
2024-08-12 16:08:15 -0700

    migrates to nuxt

---
+++ app.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import style from "./mapbox/style.json";
+
+mapboxgl.accessToken = "";
+
+const mapContainer = ref();
+
+onMounted(() => {
+  new mapboxgl.Map({
+    "container": mapContainer.value,
+    "style": style
+  });
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <div ref="mapContainer" class="map"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+.content {
+  flex-grow: 1;
+  position: relative
+}
+
+.map {
+  height: 100%;
+}
+</style>
