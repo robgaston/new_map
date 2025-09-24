c2b35f30a78d760e0b9415277f96c904ab3ae9ea
Rob Gaston <rgaston@fargeo.com>
2025-09-24 11:27:50 -0700

    replaces mapbox with maplibre and openfreemap

--- app.vue
+++ app.vue
@@ -1,26 +1,54 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import style from "./mapbox/style.json";
 
-mapboxgl.accessToken = "";
-
+const attribution = ref();
 const mapContainer = ref();
 
 onMounted(() => {
-  new mapboxgl.Map({
+  const map = new maplibregl.Map({
     "container": mapContainer.value,
-    "style": style
+    "style": style,
+    "attributionControl": false
   });
+
+  map.addControl(new maplibregl.AttributionControl({
+    compact: true,
+    customAttribution: attribution.value.innerHTML
+  }));
 });
 </script>
 
 <template>
-  <div ref="mapContainer" class="map"></div>
+  <div class="container">
+    <div class="content">
+      <div ref="mapContainer" class="map"></div>
+    </div>
+  </div>
+  <div ref="attribution" class="hidden">
+    <a href="https://maplibre.org/" target="_blank">
+      MapLibre,
+    </a>
+    <a href="https://openfreemap.org/" target="_blank">
+      OpenFreeMap
+    </a>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+.content {
+  flex-grow: 1;
+  position: relative
+}
+
 .map {
   height: 100%;
 }
